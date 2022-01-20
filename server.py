1e862701b6743095bd13bfe49e1cf4cf13453764
placebo1314 <placebo1314@gmail.com>
2022-01-20 22:37:30 +0100

    multirows fixed1

--- server.py
+++ server.py
@@ -39,6 +39,7 @@ def display_question(question_id):
             current_question.append(row[TITLE])
             current_question.append(row[MESSAGE])
             current_question.append(row[QUESTION_IMG_PATH])
+            write_file(list_of_questions, QUESTIONS_FILE)
     answer_list = read_file(ANSWERS_FILE)
     current_answers = []
     for row in answer_list:
