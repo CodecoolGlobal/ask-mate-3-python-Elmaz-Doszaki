53d3563fb0c4115214a9a204daf943b391cdd1f0
Bálint Németh <nemeth.balint1996@gmail.com>
2022-02-16 19:39:28 +0100

    Filter bonus questions js

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -36,25 +36,27 @@ function getFilteredItems(items, filterValue) {
     //     items.pop()
     // }
     const newItems = []
-    for (let i=0; i<items.length; i++) {
-        console.log(items[i])
+    for (let i = 0; i < items.length; i++) {
 
-        if (  filterValue.slice(0,13) === "!Description:"  &&  items[i]['Description'].indexOf(filterValue.slice(13)) !== -1) {
-            newItems.push(items[i])
-            console.log(newItems)
-        }
-        else if (  filterValue.slice(0,12) === "Description:" &&  items[i]['Description'].indexOf(filterValue.slice(12)) !== -1) {
-            newItems.push(items[i])
-        }
-        else if (items[i]['Title'].indexOf(filterValue) !== -1 || items[i]['Description'].indexOf(filterValue) !== -1) {
-            newItems.push(items[i])
-        }
-        else if (filterValue[0] === "!" && (items[i]['Title'].includes(filterValue.slice(1)) === false && items[i]['Description'].includes(filterValue.slice(1)) === false)){
-            newItems.push(items[i])
+        if (filterValue.slice(0, 13) === "!Description:" && items[i]['Description'].indexOf(filterValue.slice(13)) === -1) {
+
+            newItems.push(items[i]);
+
+        } else if (filterValue.slice(0, 12) === "Description:" && items[i]['Description'].indexOf(filterValue.slice(12)) !== -1) {
+
+            newItems.push(items[i]);
+
+        } else if (items[i]['Title'].indexOf(filterValue) !== -1) {
+
+            newItems.push(items[i]);
+
+        } else if (filterValue[0] === "!" && items[i]['Title'].includes(filterValue.slice(1)) === false && filterValue.slice(0, 13) !== "!Description:") {
+
+            newItems.push(items[i]);
+
         }
 
     }
-// ha ! és benne van, akkor ne mutassa, minden mást igen
     return newItems
 }
 
